ea9b2dfd9a62786bf20ea6b6e8eb18c1bbf93ed0
Yuri <32714790+Eimaen@users.noreply.github.com>
2023-04-04 15:14:23 +0200

    cosmetic changes

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -17,13 +17,18 @@ export default function Settings(): React.ReactElement {
         minValue={1}
         maxValue={25}
         stickToMarkers={true}
-        markers={[1, 5, 10, 15, 20, 25]}>
+        markers={[1, 5, 10, 15, 20, 25]}
+        note="The limit of messages to be included into the ChatGPT context.">
         Keep context of N messages
       </SliderItem>
-      <SwitchItem {...util.useSetting(cfg, "ignoreReplyChains", false)}>
+      <SwitchItem
+        {...util.useSetting(cfg, "ignoreReplyChains", false)}
+        note="When used in guild channels, by default, when manual context selection is disabled, plugin uses reply chains to get the context. It means, the target message, the message the target message was a reply on, reply on another reply and so on...">
         Don't use reply chains when not in DM channel
       </SwitchItem>
-      <SwitchItem {...util.useSetting(cfg, "manualContext", true)}>
+      <SwitchItem
+        {...util.useSetting(cfg, "manualContext", true)}
+        note="Allows you to choose the messages included into context by yourself (buggy). Right-click the OpenAI tooltip button to add/remove message from the context.">
         Use manual context selection
       </SwitchItem>
     </div>
